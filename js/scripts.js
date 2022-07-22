8951d94a37134775021dcd0eac03069353eeb77c
LastVoyager <maximvb89@gmail.com>
2022-07-23 02:46:35 +0300

    22.07-2

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@
 const element = document.getElementsByClassName('left-option');
 let first = document.querySelector(".options.burger-active .left-option li[name=first]");
 
+let sent = document.querySelector(".form-button");
+let popup = document.querySelector(".popup");
+
+
 console.log(element);
 
 $(document).ready(function(){
@@ -14,8 +18,6 @@ $(document).ready(function(){
 }); 
 
 
-var link = document.querySelector('link[rel=import]');
-var content = link.import.querySelector('#header');
-document.body.appendChild(content.cloneNode(true));
+
 
 
