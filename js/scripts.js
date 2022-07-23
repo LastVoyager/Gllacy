83ff0f7e86f71404afdcbaffe71a88aa5ed53650
LastVoyager <maximvb89@gmail.com>
2022-07-23 15:13:23 +0300

    23.07-2

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,12 @@
 const element = document.getElementsByClassName('left-option');
 let first = document.querySelector(".options.burger-active .left-option li[name=first]");
 
-let sent = document.querySelector(".form-button");
-let popup = document.querySelector(".popup");
+let radio_1 = document.querySelector(".slider-point-1");
+let radio_2 = document.querySelector(".slider-point-2");
+let radio_3 = document.querySelector(".slider-point-3");
+let image_1 = document.querySelector(".ice-cream-1");
+let image_2 = document.querySelector(".ice-cream-2");
+let image_3 = document.querySelector(".ice-cream-3");
 
 
 console.log(element);
@@ -16,6 +20,9 @@ $(document).ready(function(){
         $('body').toggleClass('lock');
     });
 }); 
+
+
+
 
 
 
