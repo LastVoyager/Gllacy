c830230268cc0dacc7590abc981cec638f7f9d03
LastVoyager <maximvb89@gmail.com>
2022-09-03 19:21:57 +0300

    03.09.-1

--- js/scripts.js
+++ js/scripts.js
@@ -22,7 +22,19 @@ $(document).ready(function(){
 }); 
 
 
-
+$(function() {
+	$( "#slider-range" ).slider({
+	  range: true,
+	  min: 0,
+	  max: 500,
+	  values: [ 0, 250 ],
+	  slide: function( event, ui ) {
+		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	  }
+	});
+	$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+	  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+});
 
 
 
